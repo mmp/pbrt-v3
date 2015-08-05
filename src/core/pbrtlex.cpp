f97dabde2920d08943c115beda4ae3bff4ceff50
Shih-Chin <shihchin.weng@gmail.com>
2015-08-05 21:39:31 +0800

    Substitute unistd.h with io.h for MSVC.

--- src/core/pbrtlex.cpp
+++ src/core/pbrtlex.cpp
@@ -801,7 +801,11 @@ void include_pop() {
  * down here because we want the user's section 1 to have been scanned first.
  * The user has a chance to override it with an option.
  */
+#ifdef _MSC_VER
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 #endif
 
 #ifndef YY_EXTRA_TYPE
