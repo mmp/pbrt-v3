7c6de2b571e499b350ab3b296801a51b708089c1
Matt Pharr <matt@pharr.org>
2016-03-04 09:02:18 -0800

    Use std::thread::hardware_concurrency() to find # cores

--- src/core/parallel.cpp
+++ src/core/parallel.cpp
@@ -36,14 +36,6 @@
 #include "parallel.h"
 #include "memory.h"
 #include "stats.h"
-#ifdef PBRT_IS_WINDOWS
-#include <windows.h>
-#else
-#include <sys/sysctl.h>
-#endif  // PBRT_IS_WINDOWS
-#ifdef PBRT_IS_LINUX
-#include <unistd.h>
-#endif  // PBRT_IS_LINUX
 #include <list>
 #include <thread>
 #include <condition_variable>
@@ -277,36 +269,7 @@ void ParallelFor(std::function<void(Point2i)> func, const Point2i &count) {
 
 int NumSystemCores() {
     if (PbrtOptions.nThreads > 0) return PbrtOptions.nThreads;
-#ifdef PBRT_IS_WINDOWS
-    SYSTEM_INFO sysinfo;
-    GetSystemInfo(&sysinfo);
-    return sysinfo.dwNumberOfProcessors;
-#elif defined(__linux__)
-    return sysconf(_SC_NPROCESSORS_ONLN);
-#else
-// mac/bsds
-#ifdef __OpenBSD__
-    int mib[2] = {CTL_HW, HW_NCPU};
-#else
-    int mib[2];
-    mib[0] = CTL_HW;
-    size_t length = 2;
-    if (sysctlnametomib("hw.logicalcpu", mib, &length) == -1) {
-        Error("sysctlnametomib() filed.  Guessing 2 CPU cores.");
-        return 2;
-    }
-    Assert(length == 2);
-#endif
-    int nCores = 0;
-    size_t size = sizeof(nCores);
-
-    /* get the number of CPUs from the system */
-    if (sysctl(mib, 2, &nCores, &size, nullptr, 0) == -1) {
-        Error("sysctl() to find number of cores present failed");
-        return 2;
-    }
-    return nCores;
-#endif
+    return std::max(1u, std::thread::hardware_concurrency());
 }
 
 void TerminateWorkerThreads() {
