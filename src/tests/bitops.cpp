85d7264c17b61aff69048b99a11c646e0cb8d257
Matt Pharr <matt@pharr.org>
2016-06-12 11:27:51 -0700

    add RoundUpPow2 tests

--- src/tests/bitops.cpp
+++ src/tests/bitops.cpp
@@ -1,7 +1,7 @@
 
-#include "tests/gtest/gtest.h"
-#include "pbrt.h"
 #include <stdint.h>
+#include "pbrt.h"
+#include "tests/gtest/gtest.h"
 
 TEST(Log2, Basics) {
     for (int i = 0; i < 32; ++i) {
@@ -32,5 +32,34 @@ TEST(CountTrailing, Basics) {
     for (int i = 0; i < 32; ++i) {
         uint32_t ui = 1u << i;
         EXPECT_EQ(i, CountTrailingZeros(ui));
+    }
+}
+
+TEST(RoundUpPow2, Basics) {
+    EXPECT_EQ(RoundUpPow2(7), 8);
+    for (int i = 1; i < (1 << 24); ++i)
+        if (IsPowerOf2(i))
+            EXPECT_EQ(RoundUpPow2(i), i);
+        else
+            EXPECT_EQ(RoundUpPow2(i), 1 << (Log2Int(i) + 1));
+
+    for (int64_t i = 1; i < (1 << 24); ++i)
+        if (IsPowerOf2(i))
+            EXPECT_EQ(RoundUpPow2(i), i);
+        else
+            EXPECT_EQ(RoundUpPow2(i), 1 << (Log2Int(i) + 1));
+
+    for (int i = 0; i < 30; ++i) {
+        int v = 1 << i;
+        EXPECT_EQ(RoundUpPow2(v), v);
+        if (v > 2) EXPECT_EQ(RoundUpPow2(v - 1), v);
+        EXPECT_EQ(RoundUpPow2(v + 1), 2 * v);
+    }
+
+    for (int i = 0; i < 62; ++i) {
+        int64_t v = 1ll << i;
+        EXPECT_EQ(RoundUpPow2(v), v);
+        if (v > 2) EXPECT_EQ(RoundUpPow2(v - 1), v);
+        EXPECT_EQ(RoundUpPow2(v + 1), 2 * v);
     }
 }
