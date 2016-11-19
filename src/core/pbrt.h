6a590d5717cb8a7525b9cf8a8753acad5cf0bb0b
Matt Pharr <matt@pharr.org>
2016-11-19 09:16:49 -0800

    Use constexpr for a few more constants.
    Issue #95.

--- src/core/pbrt.h
+++ src/core/pbrt.h
@@ -172,14 +172,14 @@ static PBRT_CONSTEXPR Float Infinity = std::numeric_limits<Float>::infinity();
 static PBRT_CONSTEXPR Float MachineEpsilon =
     std::numeric_limits<Float>::epsilon() * 0.5;
 #endif
-const Float ShadowEpsilon = 0.0001f;
-static const Float Pi = 3.14159265358979323846;
-static const Float InvPi = 0.31830988618379067154;
-static const Float Inv2Pi = 0.15915494309189533577;
-static const Float Inv4Pi = 0.07957747154594766788;
-static const Float PiOver2 = 1.57079632679489661923;
-static const Float PiOver4 = 0.78539816339744830961;
-static const Float Sqrt2 = 1.41421356237309504880;
+static PBRT_CONSTEXPR Float ShadowEpsilon = 0.0001f;
+static PBRT_CONSTEXPR Float Pi = 3.14159265358979323846;
+static PBRT_CONSTEXPR Float InvPi = 0.31830988618379067154;
+static PBRT_CONSTEXPR Float Inv2Pi = 0.15915494309189533577;
+static PBRT_CONSTEXPR Float Inv4Pi = 0.07957747154594766788;
+static PBRT_CONSTEXPR Float PiOver2 = 1.57079632679489661923;
+static PBRT_CONSTEXPR Float PiOver4 = 0.78539816339744830961;
+static PBRT_CONSTEXPR Float Sqrt2 = 1.41421356237309504880;
 #if defined(PBRT_IS_MSVC)
 #define alloca _alloca
 #endif
