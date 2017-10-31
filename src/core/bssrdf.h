fab73481b835f5e022f043538d6c410af3aab7f6
Matt Pharr <matt@pharr.org>
2017-10-31 07:47:33 -0700

    Add virtual destructor to BSSRDF class.

--- src/core/bssrdf.h
+++ src/core/bssrdf.h
@@ -54,6 +54,7 @@ class BSSRDF {
   public:
     // BSSRDF Public Methods
     BSSRDF(const SurfaceInteraction &po, Float eta) : po(po), eta(eta) {}
+    virtual ~BSSRDF() {}
 
     // BSSRDF Interface
     virtual Spectrum S(const SurfaceInteraction &pi, const Vector3f &wi) = 0;
