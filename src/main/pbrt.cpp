f9a8b05db82e1be0c54ba3da7987281cddbe2bfe
Matt Pharr <matt@pharr.org>
2018-01-07 15:17:38 -0800

    Bump printed copyright to 2018

--- src/main/pbrt.cpp
+++ src/main/pbrt.cpp
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
         printf("*** DEBUG BUILD ***\n");
 #endif // !NDEBUG
         printf(
-            "Copyright (c)1998-2016 Matt Pharr, Greg Humphreys, and Wenzel "
+            "Copyright (c)1998-2018 Matt Pharr, Greg Humphreys, and Wenzel "
             "Jakob.\n");
         printf(
             "The source code to pbrt (but *not* the book contents) is covered "
