512d34fee4ff5a47acef0b3cbf1b49d8810a69c6
giuliojiang <giuliojiang@gmail.com>
2018-04-06 17:08:32 +0200

    Wire up websocket and subprocess

--- tools/interactive_training_view/server/index.js
+++ tools/interactive_training_view/server/index.js
@@ -2,6 +2,9 @@ var express = require("express")
 var app = express()
 var path = require("path")
 var WebSocket = require("ws");
+const {spawn} = require("child_process");
+
+var priv = {};
 
 // ============================================================================
 // Constants
@@ -33,14 +36,73 @@ var wss = new WebSocket.Server({
     port: WS_PORT
 });
 
-var handle_ws_message = function(ws, data) {
+priv.handle_ws_message = function(ws, data) {
     var msgobj = JSON.parse(data);
     var t = msgobj._t;
-    console.info("Got a ["+ t +"] message");
+    var handler_name = "handler_" + t;
+    var handler = priv[handler_name];
+    if (!handler) {
+        console.info("No handler for type " + t);
+        return;
+    }
+    handler(msgobj);
 };
 
 wss.on("connection", function(ws) {
     ws.on("message", function(data) {
-        handle_ws_message(ws, data);
+        priv.handle_ws_message(ws, data);
     });
 });
+
+// ============================================================================
+// Main handlers
+
+priv.handler_button_load = function(msgobj) {
+    console.info("Load");
+    priv.start_python_subprocess();
+};
+
+priv.handler_button_next = function(msgobj) {
+    console.info("Next");
+};
+
+// ============================================================================
+// Python process management
+
+// Subprocess data
+prc = {};
+
+function emitLines (stream) {
+    var backlog = ''
+    stream.on('data', function (data) {
+        backlog += data
+        var n = backlog.indexOf('\n')
+        // got a \n? emit one or more 'line' events
+        while (~n) {
+        stream.emit('line', backlog.substring(0, n))
+        backlog = backlog.substring(n + 1)
+        n = backlog.indexOf('\n')
+        }
+    })
+    stream.on('end', function () {
+        if (backlog) {
+        stream.emit('line', backlog)
+        }
+    })
+};
+
+priv.start_python_subprocess = function() {
+    var interactive_py_path = path.join(__dirname, "..", "..", "..", "ml", "main_interactive_view.py");
+    prc.proc = spawn("python3", [interactive_py_path]);
+    prc.proc.stdout.on("line", (line) => {
+        console.info("STDOUT: ["+ line +"]");
+    });
+    emitLines(prc.proc.stdout);
+    prc.proc.stderr.on("line", (line) => {
+        console.info("STDERR: ["+ line +"]");
+    });
+    prc.proc.on("close", function(code, signal) {
+        console.info("Exited with ["+ code +"] ["+ signal +"]");
+        prc.proc = null;
+    });
+};
