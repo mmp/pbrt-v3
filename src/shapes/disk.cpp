2f0852ce2f6bcdb82f8305229ffd5aecff8edcfd
Matt Pharr <matt@pharr.org>
2018-12-29 16:31:10 -0800

    Try two at disk parameterization and partial derivatives.
    The fix from issue #154 led to a different bug (issue #183). This switches
    back to the old partial derivative vectors via adjusting the disk
    parameterization. (Now (0,0) is on the outer ring, which isn't great, but
    seems like the least worst option.)

--- src/shapes/disk.cpp
+++ src/shapes/disk.cpp
@@ -73,8 +73,7 @@ bool Disk::Intersect(const Ray &r, Float *tHit, SurfaceInteraction *isect,
     // Find parametric representation of disk hit
     Float u = phi / phiMax;
     Float rHit = std::sqrt(dist2);
-    Float oneMinusV = ((rHit - innerRadius) / (radius - innerRadius));
-    Float v = 1 - oneMinusV;
+    Float v = (radius - rHit) / (radius - innerRadius);
     Vector3f dpdu(-phiMax * pHit.y, phiMax * pHit.x, 0);
     Vector3f dpdv =
         Vector3f(pHit.x, pHit.y, 0.) * (innerRadius - radius) / rHit;
