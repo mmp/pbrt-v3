mmp/pbrt-v3

commit 8445c002c26e76d8924436a609b0c519095c5c15

SPPM: move MemoryArena vector out of "nIterations" loop
This saves a bunch of unnecessary MemoryArena creation and deletion, which in turn was hurting parallel scalability.

src/integrators/sppm.cpp
@@ -130,9 +130,9 @@ void SPPMIntegrator::Render(const Scene &scene) {
     Point2i nTiles((pixelExtent.x + tileSize - 1) / tileSize,
                    (pixelExtent.y + tileSize - 1) / tileSize);
     ProgressReporter progress(2 * nIterations, "Rendering");
+    std::vector<MemoryArena> perThreadArenas(MaxThreadIndex());
     for (int iter = 0; iter < nIterations; ++iter) {
         // Generate SPPM visible points
-        std::vector<MemoryArena> perThreadArenas(MaxThreadIndex());
         {
             ProfilePhase _(Prof::SPPMCameraPass);
             ParallelFor2D([&](Point2i tile) {

commit 416a080082a27e1f204e31ffe58e5e5cab2d179c

Fix memory leak introduced in 8445c002c.
Issue #264.

src/integrators/sppm.cpp
@@ -495,6 +495,10 @@ void SPPMIntegrator::Render(const Scene &scene) {
                 WriteImage("sppm_radius.png", rimg.get(), pixelBounds, res);
             }
         }
+
+        // Reset memory arenas
+        for (int i = 0; i < perThreadArenas.size(); ++i)
+            perThreadArenas[i].Reset();
     }
     progress.Done();
 }