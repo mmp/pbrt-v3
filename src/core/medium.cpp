3123d856067aebe201840dde3dd80c26535534d9
Matt Pharr <matt@pharr.org>
2020-06-26 13:36:10 -0700

    Fix Henyey Greenstein importance sampling sign conventions (via Jiayin Cao)

--- src/core/medium.cpp
+++ src/core/medium.cpp
@@ -199,8 +199,8 @@ Float HenyeyGreenstein::Sample_p(const Vector3f &wo, Vector3f *wi,
     if (std::abs(g) < 1e-3)
         cosTheta = 1 - 2 * u[0];
     else {
-        Float sqrTerm = (1 - g * g) / (1 - g + 2 * g * u[0]);
-        cosTheta = (1 + g * g - sqrTerm * sqrTerm) / (2 * g);
+        Float sqrTerm = (1 - g * g) / (1 + g - 2 * g * u[0]);
+        cosTheta = -(1 + g * g - sqrTerm * sqrTerm) / (2 * g);
     }
 
     // Compute direction _wi_ for Henyey--Greenstein sample
@@ -208,8 +208,8 @@ Float HenyeyGreenstein::Sample_p(const Vector3f &wo, Vector3f *wi,
     Float phi = 2 * Pi * u[1];
     Vector3f v1, v2;
     CoordinateSystem(wo, &v1, &v2);
-    *wi = SphericalDirection(sinTheta, cosTheta, phi, v1, v2, -wo);
-    return PhaseHG(-cosTheta, g);
+    *wi = SphericalDirection(sinTheta, cosTheta, phi, v1, v2, wo);
+    return PhaseHG(cosTheta, g);
 }
 
 Float HenyeyGreenstein::p(const Vector3f &wo, const Vector3f &wi) const {
